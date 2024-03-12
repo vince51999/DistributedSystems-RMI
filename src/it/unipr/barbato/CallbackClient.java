8621e923c8e0c93aa19629135dda5606334dc1ba
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-12 16:13:10 +0100

    Created method to get random sn

--- src/it/unipr/barbato/CallbackClient.java
+++ src/it/unipr/barbato/CallbackClient.java
@@ -2,4 +2,8 @@ package it.unipr.barbato;
 
 public class CallbackClient {
 
+	public static int getRandomSN(ArrayList<Integer> array) {
+	    int rnd = new Random().nextInt(array.size());
+	    return array.get(rnd);
+	}
 }
