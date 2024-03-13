031ede2f37917ed608a976ac48ba7ba855dbd3da
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-13 16:03:55 +0100

    Creted javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"RMI","l":"it.unipr.barbato.Controller"},{"m":"RMI","l":"it.unipr.barbato.Model.Impl"},{"m":"RMI","l":"it.unipr.barbato.Model.Interface"}];updateSearchResults();
